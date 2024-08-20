0bedfd556eb2c4ca89c8ebcb4fe093c66e72bbbd
Wolker <contato@wolker.com.br>
2024-08-20 15:31:00 -0300

    Adusted backgrounds

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,23 +1,23 @@
 import React from 'react'
 
-import theme from '../../Theme.module.css'
+// import theme from '../../Theme.module.css'
 import styles from './Home.module.css'
 
 export const Home = () => {
   return (
-    <div className={theme.content}>
-      <div className={styles.home}>
-        <div className={styles.leftSide}>
-          <b>Luis Filipe Severgnini</b>
-          <p>GERENTE DE PROJETOS</p>
-        </div>
+    // <div className={theme.content}>
+    <div className={styles.home}>
+      <div className={styles.leftSide}>
+        <b>Luis Filipe Severgnini</b>
+        {/* <p>GERENTE DE PROJETOS</p> */}
+      </div>
 
-        <div className={styles.rightSide}>
-          <h1>Prazer!</h1>
-          <b>Meu perfil e meus projetos</b>
-          <p>Sou ...</p>
-        </div>
+      <div className={styles.rightSide}>
+        <h1>Prazer!</h1>
+        {/* <b>Meu perfil e meus projetos</b>
+          <p>Sou ...</p> */}
       </div>
     </div>
+    // </div>
   )
 }
